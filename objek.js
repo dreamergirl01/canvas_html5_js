d33bf2d7de9879f5ebdc04c268bc73e79f08647b
dreamergirl01 <rahma01twins@gmail.com>
2025-01-12 23:25:22 +0700

    creating classes and object circles dynamic

---
+++ objek.js
@@ -0,0 +1,41 @@
+const myCanvas = document.getElementById("myCanvas")
+const context = myCanvas.getContext("2d")
+
+var window_height = window.innerHeight
+var window_width = window.innerWidth
+
+myCanvas.width = window_width
+myCanvas.height = window_height
+
+myCanvas.style.background = "#C2B280"
+
+class Circle{
+    constructor(x, y, radius, color){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw(context){
+        context.beginPath()
+        context.lineWidth = 5
+        context.arc(this.x, this.y, this.radius, 0, 2*Math.PI, false)
+        context.stroke()
+        context.closePath()
+    }
+}
+
+let all_circles = []
+
+let createCircle = function(my_circle){
+    my_circle.draw(context)
+}
+
+for (var numbers = 0; numbers < 10; numbers++){
+    let random_x = Math.random()* window_width
+    let random_y = Math.random() * window_width
+    let my_circle = new Circle(random_x, random_y, 50, "black")
+    all_circles.push(my_circle)
+    createCircle(all_circles[numbers])
+}
