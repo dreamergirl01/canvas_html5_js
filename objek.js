ba958ebd68c0b795beae9a663ddf0c2ef84e2f2c
dreamergirl01 <rahma01twins@gmail.com>
2025-01-13 22:27:28 +0700

    update object animation and collision detection

--- objek.js
+++ objek.js
@@ -10,12 +10,15 @@ myCanvas.height = window_height
 myCanvas.style.background = "#C2B280"
 
 class Circle{
-    constructor(x, y, radius, color, text){
+    constructor(x, y, radius, color, text, speed){
         this.x = x
         this.y = y
         this.radius = radius
         this.color = color
         this.text = text
+        this.speed = speed
+        this.dx = 1 * this.speed
+        this.dy = 1 * this.speed
     }
 
     draw(context){
@@ -33,20 +36,58 @@ class Circle{
         context.stroke()
         context.closePath()
     }
+
+    update(){
+        context.clearRect(0, 0, window_width, window_height)
+
+        this.draw(context)
+
+        if((this.x + this.radius) > window_width){
+            this.dx = -this.dx
+        }
+        if((this.x - this.radius) < 0){
+            this.dx = -this.dx
+        }
+        if((this.y + this.radius) > window_width){
+            this.dy = -this.dy
+        }
+        if((this.y - this.radius) < 0){
+            this.dy = -this.dy
+        }
+
+        this.x += this.dx
+        this.y += this.dy
+    }
 }
 
+//creating class, object and font
+// let circle_counter = 1
+// let all_circles = []
+
+// let createCircle = function(my_circle){
+//     my_circle.draw(context)
+// }
+
+// for (var numbers = 0; numbers < 1; numbers++){
+//     let random_x = Math.random()* window_width
+//     let random_y = Math.random() * window_width
+//     let my_circle = new Circle(random_x, random_y, 50, "black", circle_counter,1)
+//     all_circles.push(my_circle)
+//     createCircle(all_circles[numbers])
+//     circle_counter++
+// }
+
+//moving object animation +collicion detection
 let circle_counter = 1
-let all_circles = []
+let random_x = Math.random() * window_width
+let random_y = Math.random() * window_width
 
-let createCircle = function(my_circle){
-    my_circle.draw(context)
+let my_circle = new Circle(random_x, random_y, 50, "black", circle_counter, 2)
+my_circle.draw(context)
+
+let updateCircle = function(){
+    requestAnimationFrame(updateCircle)
+    my_circle.update()
 }
 
-for (var numbers = 0; numbers < 10; numbers++){
-    let random_x = Math.random()* window_width
-    let random_y = Math.random() * window_width
-    let my_circle = new Circle(random_x, random_y, 50, "black", circle_counter)
-    all_circles.push(my_circle)
-    createCircle(all_circles[numbers])
-    circle_counter++
-}
+updateCircle()
